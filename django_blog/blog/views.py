052602e97f8bc1315806d53eaf7e54b825986d8f
Umugwaneza sandrine <umusandrine001@gmail.com>
2025-12-05 09:40:36 +0200

    some...

---
+++ django_blog/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 10
